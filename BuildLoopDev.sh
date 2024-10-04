a7821fbd87afbfc03cde458810a75792e4def489
marionbarker <marionbarker@earthlink.net>
2024-10-04 13:22:24 -0700

    improve the Xcode 16 speed-up method and information messages

--- BuildLoopDev.sh
+++ BuildLoopDev.sh
@@ -867,12 +867,11 @@ check_config_override_existence_offer_to_configure
 ensure_a_year
 
 ############################################################
-# temporary shell script to patch Xcode-16 problems
+# temporary shell script to patch Xcode 16 problems
+# valid for Xcode 15 as well
 ############################################################
 
-echo -e "There are Xcode 16 issues that will be patched with the next few commands"
-echo -e "If you see an error it is possible that the released code has been updated"
-echo ""
+section_divider
 script_name="xcode-16-fixes.sh"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/loopandlearn/lnl-scripts/$SCRIPT_BRANCH/$script_name)"
 return_when_ready
--- xcode-16-fixes.sh
+++ xcode-16-fixes.sh
@@ -1,14 +1,25 @@
 # These patches apply fixes to Xcode, mostly need for Xcode 16
+echo "Some Xcode issues are patched with the next few commands."
+echo "  Code will build fine with Xcode 15.4 as well as 16.x."
+echo ""
+echo "  If a PR patch gives an error - the PR was probably merged and script not yet updated."
+echo "   You can ignore the error."
+echo ""
 # fix logging error that shows up in Xcode debug display
+echo "1. Apply a patch taken from LoopWorkspace PR 183"
+echo "   - fixes a reported error in Xcode debug log"
+echo ""
 curl https://github.com/loopandlearn/LoopWorkspace/commit/a016f9d73d12b62c7bb5ce00eb19c1cbd75fa12e.patch | git apply
+echo ""
 # update provisioning profile so expiration date is recorded
+# Note that PR 2226 also modifies the version number, but this patch only fixes expiration date
+echo "2. Apply a patch similar to Loop PR 2226"
+echo "   - Xcode 16 fix for reporting expiration date"
+echo ""
 curl https://github.com/loopandlearn/Loop/commit/15f73c69f8fc0f14b34989e0820007fbbfca5acf.patch | git apply --directory=Loop
+echo ""
 # make updates to submodules that speed up initial build by about 6 minutes
-curl https://github.com/dnzxy/AmplitudeService/commit/99273f5e78b2ff0563e2275c601c9fc2230327f8.patch | git apply --directory=AmplitudeService
-curl https://github.com/dnzxy/AmplitudeService/commit/6f406e0100e5e4b83816d48831470a59e48d4467.patch | git apply --directory=AmplitudeService
-curl https://github.com/dnzxy/LibreTransmitter/commit/5983923cfcf8cd5f0f58758ec45b9a3450d300c8.patch | git apply --directory=LibreTransmitter
-curl https://github.com/dnzxy/LoopKit/commit/49df814edd30a839c0afda1ece6fe9972d40aed6.patch | git apply --directory=LoopKit
-curl https://github.com/dnzxy/MixpanelService/commit/1b834dfc7e45e94f895f77125efa016fad3b540b.patch | git apply --directory=MixpanelService
-curl https://github.com/dnzxy/MixpanelService/commit/6aaa1506073bc99e7329373b489bc36856b77a9b.patch | git apply --directory=MixpanelService
-curl https://github.com/dnzxy/OmniBLE/commit/cafb86628d741c74fa77dfcdec1a1b5def78bde5.patch | git apply --directory=OmniBLE
-curl https://github.com/dnzxy/OmniKit/commit/0232f95015f1255722c5c08ab7c32a094111ef15.patch | git apply --directory=OmniKit
+echo "3. Make modifications that speed up building with Xcode 16"
+find . -name "*.pbxproj" -exec sed -i '' 's/ENABLE_MODULE_VERIFIER = YES;/ENABLE_MODULE_VERIFIER = "$(CLANG_MODULES_ENABLE_VERIFIER_TOOL)";/g' {} +
+echo ""
+echo "Done with patches"
